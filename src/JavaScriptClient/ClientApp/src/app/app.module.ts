0a2478f409071014b3175f516ca5d93e19a98f7b
Jorge González <jegnzc@gmail.com>
2022-09-05 00:41:34 -0600

    Added navbar and menu to angular

--- src/JavaScriptClient/ClientApp/src/app/app.module.ts
+++ src/JavaScriptClient/ClientApp/src/app/app.module.ts
@@ -4,15 +4,25 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MenuComponent } from './menu/menu.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+import { MaterialExampleModule } from '../material.module';
 
 @NgModule({
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialExampleModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MenuComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
