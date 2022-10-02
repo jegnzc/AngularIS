d8e4723166d26066229f1dade1e2cee2d8c09fc2
Jorge González <jegnzc@gmail.com>
2022-10-02 16:30:21 -0600

    ADded authGuard

--- src/JavaScriptClient/ClientApp/src/app/app-routing.module.ts
+++ src/JavaScriptClient/ClientApp/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//Guard
+import { AuthGuard } from './shared/auth.guard';
+const routes: Routes = [
+  { path: '', canActivate: [AuthGuard] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
